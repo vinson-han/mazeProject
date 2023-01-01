acf41caa3e8b0f65b98312452c9692e03e8f75b5
vinson-han <vinson.han21@gmail.com>
2022-12-31 18:41:18 -0800

    tiles

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
-
+var grid = []
+var current, rows, cols
+var w = 50
 
 const button = document.getElementById('button')
 
@@ -15,18 +17,62 @@ getValueFromButton()
 function setup() {
 	let cnv = createCanvas(500, 500)
 	cnv.parent('myContainer')
+	cols = floor(width / 20)
+	rows = floor(height / 20)
+
+	for (let j = 0; j < rows; j++) {
+		for (let i = 0; i < cols; i++) {
+			let cell = new Cell(i, j)
+			grid.push(cell)
+		}
+	}
 }
 
 function draw() {
-	background(0, 100, 200)
+	background(51)
+
+	for (let i = 0; i < grid.length; i++) {
+		grid[i].show()
+	}
+
 }
 
 
 function makeMaze(w, h) {
-	console.log('hi')
-	resizeCanvas(w * 100, h * 100)
+	resizeCanvas(w * 250, h * 250)
+	cols = floor(width / 20)
+	rows = floor(height / 20)
+
+	for (let j = 0; j < rows; j++) {
+		for (let i = 0; i < cols; i++) {
+			let cell = new Cell(i, j)
+			grid.push(cell)
+		}
+	}
+
+	for (let i = 0; i < grid.length; i++) {
+		grid[i].show()
+	}
+
+
 }
 
 function makeTile() {
 
+}
+
+function Cell(i, j) {
+	this.i = i
+	this.j = j
+	this.wall = [true, true, true, true]
+	this.visited = false
+
+
+	this.show = function () {
+		var x = this.i * w
+		var y = this.j * w
+		stroke(255)
+		noFill()
+		rect(x, y, w, w)
+	}
 }
